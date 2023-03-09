e8b9dfcf6a37a054386ef70c6a71829122fc5c5b
mrkwrght <markwright785@gmail.com>
2023-03-09 13:36:41 +0000

    trying to fit images

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,17 @@ module.exports = function (eleventyConfig) {
   // Passthrough
   eleventyConfig.addPassthroughCopy({ './app/images': '.' })
 
+  const Image = require("@11ty/eleventy-img");
+
+(async () => {
+  let url = "https://images.unsplash.com/photo-1608178398319-48f814d0750c";
+  let stats = await Image(url, {
+    widths: [300]
+  });
+
+  console.log( stats );
+})();
+
   // Config
   return {
     dataTemplateEngine: 'njk',

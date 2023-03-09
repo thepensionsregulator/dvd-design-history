ed093a284887c7a9a293dc60e731043229b2a4af
mrkwrght <markwright785@gmail.com>
2023-03-09 14:35:34 +0000

    images

--- .eleventy.js
+++ .eleventy.js
@@ -25,29 +25,4 @@ module.exports = function (eleventyConfig) {
   })
 
   // Passthrough
-  eleventyConfig.addPassthroughCopy({ './app/images': '.' })
-
-  const Image = require("@11ty/eleventy-img");
-
-(async () => {
-  let url = "https://images.unsplash.com/photo-1608178398319-48f814d0750c";
-  let stats = await Image(url, {
-    widths: [300]
-  });
-
-  console.log( stats );
-})();
-
-  // Config
-  return {
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
-    dir: {
-      input: 'app',
-      output: 'public',
-      layouts: '_layouts',
-      includes: '_components'
-    }
-  }
-}
+  eleventyConfig.addPassthroughCopy({ './app/assets/images': '.' })
